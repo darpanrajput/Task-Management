1b03446744218d5bef489b6ed972db7dc896e100
darpan rajput <darpanrajput412@gmail.com>
2026-03-01 00:36:11 +0530

    added view

--- backend/src/middlewares/rateLimiter.js
+++ backend/src/middlewares/rateLimiter.js
@@ -1,7 +1,7 @@
 import rateLimit from "express-rate-limit";
 
 export const loginLimiter = rateLimit({
-    windowMs: 15 * 60 * 1000,
+    windowMs: 5 * 60 * 1000,
     max: 5,
     message: "Too many login attempts, please try again after 15 minutes",
     standardHeaders: true,
