b422bd89ecb5aa5e689e5037da13c9355d1ad323
darpan rajput <darpanrajput412@gmail.com>
2026-02-28 15:54:09 +0530

    build ailed

---
+++ frontend/src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <main className="app-shell">
+      <h1>Task Management</h1>
+      <p>Frontend scaffold is restored.</p>
+    </main>
+  )
+}
+
+export default App
---
+++ frontend/src/Root.jsx
@@ -0,0 +1,8 @@
+import './index.css'
+import App from './App.jsx'
+
+function Root() {
+  return <App />
+}
+
+export default Root
